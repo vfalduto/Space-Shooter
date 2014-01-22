9c2e12ba3640f2fc8ea4b83db7c133eae3cade60
Vincent Falduto <vincent.falduto@gmail.com>
2014-01-22 01:17:19 +0100

    Finishing tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,22 @@ public class PlayerController : MonoBehaviour
 	public float tilt;
 	public Boundary boundary;
 	
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	
+	private float nextFire;
+	
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			audio.Play ();
+		}
+	}
+	
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.angularVelocity = Random.insideUnitCircle * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
